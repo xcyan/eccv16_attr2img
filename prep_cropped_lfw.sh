cb0601026a2c51b01343ee2ac11fcb221daf82f0
Xinchen Yan <xcyan@umich.edu>
2016-10-07 14:27:51 -0400

    Update prep_cropped_lfw.sh

--- prep_cropped_lfw.sh
+++ prep_cropped_lfw.sh
@@ -1,4 +1,4 @@
 mkdir ./data
-wget -O data/lfw_attr2img.zip https://drive.google.com/open?id=0B9Q4vh7pPrOOdUU0dS1XOWJiTWM
+wget -O data/lfw_attr2img.zip https://dl.dropboxusercontent.com/u/483000889/attr2img/lfw_attr2img.zip
 unzip data/lfw_attr2img.zip -d data/
 
