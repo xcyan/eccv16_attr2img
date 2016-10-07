001883ba504b396cfae0f331415a4e5f31b308f5
Xinchen Yan <xcyan@umich.edu>
2016-10-07 14:28:48 -0400

    Update prep_cropped_cub.sh

--- prep_cropped_cub.sh
+++ prep_cropped_cub.sh
@@ -1,4 +1,4 @@
 mkdir ./data
-wget -O data/cub_attr2img.zip https://drive.google.com/open?id=0B9Q4vh7pPrOOWHFyX0NpUXRCUzA
+wget -O data/cub_attr2img.zip https://dl.dropboxusercontent.com/u/483000889/attr2img/cub_attr2img.zip
 unzip data/cub_attr2img.zip -d data/
 
