f404fcc3373774de244f4a95820814a6059d590a
Xinchen Yan <xcyan@umich.edu>
2017-10-09 22:10:30 -0400

    Create download_preprocessed_celeba.sh

---
+++ download_preprocessed_celeba.sh
@@ -0,0 +1,4 @@
+mkdir ./data
+wget -O data/celeba_train.t7 https://umich.box.com/shared/static/sbxk3xico5w8w876eghnr6zyn0qj4eho.t7
+wget -O data/celeba_val.t7 https://umich.box.com/shared/static/hr3h31hgh2jdssnmcsxnox5n3ngywpie.t7
+wget -O data/celeba_test.t7 https://umich.box.com/shared/static/pk1lzeac4bb537lh68qcqgcjijcklpdb.t7
